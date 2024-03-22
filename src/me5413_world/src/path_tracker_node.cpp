4bec42e1e817d6e8a72753fdea7f9a28a445b9ac
ruziniuuuuu <ruziniuuuuu@gmail.com>
2024-03-22 19:48:31 +0800

    feat: Init PurePursuit, LQR, and MPC

--- src/me5413_world/src/path_tracker_node.cpp
+++ src/me5413_world/src/path_tracker_node.cpp
@@ -19,7 +19,10 @@ double PID_Kp, PID_Ki, PID_Kd;
 double STANLEY_K;
 bool PARAMS_UPDATED;
 
-void dynamicParamCallback(const me5413_world::path_trackerConfig& config, uint32_t level)
+// Method
+int METHOD = 0; // 0 for Stanley (given as default), 1 for Pure Pursuit, 2 for LQR, and 3 for MPC
+
+void dynamicParamCallback(me5413_world::path_trackerConfig& config, uint32_t level)
 {
   // Common Params
   SPEED_TARGET = config.speed_target;
@@ -72,7 +75,35 @@ double PathTrackerNode::computeStanleyControl(const double heading_error, const 
   const double stanley_output = -1.0*(heading_error + std::atan2(STANLEY_K*cross_track_error, std::max(velocity, 0.3)));
 
   return std::min(std::max(stanley_output, -2.2), 2.2);
-}
+};
+
+double PathTrackerNode::computePurePursuitControl(const double heading_error, const double cross_track_error, const double velocity)
+{
+  const double stanley_output = -1.0*(heading_error + std::atan2(STANLEY_K*cross_track_error, std::max(velocity, 0.3)));
+
+  return std::min(std::max(stanley_output, -2.2), 2.2);
+};
+
+double PathTrackerNode::computeLQRControl(const double heading_error, const double cross_track_error, const double velocity)
+{
+  // LQR Control
+  const double A = 0.5;
+  const double B = 0.5;
+  const double C = 0.5;
+  const double D = 0.5;
+
+  const double lqr_output = -1.0*(A*heading_error + B*cross_track_error + C*velocity + D*velocity);
+
+  return std::min(std::max(lqr_output, -2.2), 2.2);
+};
+
+double PathTrackerNode::computeMPCControl(const double heading_error, const double cross_track_error, const double velocity)
+{
+  // MPC Control
+  const double mpc_output = -1.0*(heading_error + cross_track_error + velocity);
+
+  return std::min(std::max(mpc_output, -2.2), 2.2);
+};
 
 geometry_msgs::Twist PathTrackerNode::computeControlOutputs(const nav_msgs::Odometry& odom_robot, const geometry_msgs::Pose& pose_goal)
 {
@@ -86,8 +117,8 @@ geometry_msgs::Twist PathTrackerNode::computeControlOutputs(const nav_msgs::Odom
   double roll, pitch, yaw_robot, yaw_goal;
   m_robot.getRPY(roll, pitch, yaw_robot);
   m_goal.getRPY(roll, pitch, yaw_goal);
-
-  const double heading_error = unifyAngleRange(yaw_robot - yaw_goal);
+  
+  const double heading_error = yaw_robot - yaw_goal;
 
   // Lateral Error
   tf2::Vector3 point_robot, point_goal;
@@ -104,44 +135,35 @@ geometry_msgs::Twist PathTrackerNode::computeControlOutputs(const nav_msgs::Odom
   const double velocity = robot_vel.length();
 
   geometry_msgs::Twist cmd_vel;
-  if (PARAMS_UPDATED)
-  {
-    this->pid_.updateSettings(PID_Kp, PID_Ki, PID_Kd);
-    PARAMS_UPDATED = false;
+
+  if (METHOD == 0) {
+    if (PARAMS_UPDATED)
+    {
+      this->pid_.updateSettings(PID_Kp, PID_Ki, PID_Kd);
+      PARAMS_UPDATED = false;
+    }
+    cmd_vel.linear.x = this->pid_.calculate(SPEED_TARGET, velocity);
+    cmd_vel.angular.z = computeStanleyControl(heading_error, lat_error, velocity);
+  } else
+  if (METHOD == 1) {
+    cmd_vel.linear.x = SPEED_TARGET;
+    cmd_vel.angular.z = computePurePursuitControl(heading_error, lat_error, velocity);
+  } else
+  if (METHOD == 2) {
+    cmd_vel.linear.x = SPEED_TARGET;
+    cmd_vel.angular.z = computeLQRControl(heading_error, lat_error, velocity);
+  } else
+  if (METHOD == 3) {
+    cmd_vel.linear.x = SPEED_TARGET;
+    cmd_vel.angular.z = computeMPCControl(heading_error, lat_error, velocity);
+  } else {
+    // Wrong Method, then stop
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angular.z = 0.0;
   }
-  cmd_vel.linear.x = this->pid_.calculate(SPEED_TARGET, velocity);
-  cmd_vel.angular.z = computeStanleyControl(heading_error, lat_error, velocity);
 
   // std::cout << "robot velocity is " << velocity << " throttle is " << cmd_vel.linear.x << std::endl;
   // std::cout << "lateral error is " << lat_error << " heading_error is " << heading_error << " steering is " << cmd_vel.angular.z << std::endl;
-
-  return cmd_vel;
-}
-
-geometry_msgs::Twist PathTrackerNode::computeControlOutputs_lqr(const nav_msgs::Odometry& odom_robot, const geometry_msgs::Pose& pose_goal)
-{
-  geometry_msgs::Twist cmd_vel;
-  if (PARAMS_UPDATED)
-  {
-    this->pid_.updateSettings(PID_Kp, PID_Ki, PID_Kd);
-    PARAMS_UPDATED = false;
-  }
-  cmd_vel.linear.x = 1;
-  cmd_vel.angular.z = 1;
-
-  return cmd_vel;
-}
-
-geometry_msgs::Twist PathTrackerNode::computeControlOutputs_mpc(const nav_msgs::Odometry& odom_robot, const geometry_msgs::Pose& pose_goal)
-{
-  geometry_msgs::Twist cmd_vel;
-  if (PARAMS_UPDATED)
-  {
-    this->pid_.updateSettings(PID_Kp, PID_Ki, PID_Kd);
-    PARAMS_UPDATED = false;
-  }
-  cmd_vel.linear.x = 1;
-  cmd_vel.angular.z = 1;
 
   return cmd_vel;
 }
